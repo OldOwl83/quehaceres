2751629066341eb112885339d7f1ddafbb838128
Mauro Donnantuoni Moratto <maurodonnamora@gmail.com>
2024-01-25 23:37:13 -0300

    Add ssl to db connection

--- src/main/db/dbConnection.js
+++ src/main/db/dbConnection.js
@@ -7,9 +7,19 @@ const client = new Client({
     port: import.meta.env.MAIN_VITE_DB_PORT,
     database: import.meta.env.MAIN_VITE_DATABASE,
     user: import.meta.env.MAIN_VITE_DB_USER,
-    password: import.meta.env.MAIN_VITE_DB_PASS
+    password: import.meta.env.MAIN_VITE_DB_PASS,
+    ssl: {
+        rejectUnauthorized: false,
+        // ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+        // key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+        // cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+      },
 })
 
-client.connect();
+try {
+    client.connect();
+} catch( err ) {
+    throw err;
+}
 
 export { client };
